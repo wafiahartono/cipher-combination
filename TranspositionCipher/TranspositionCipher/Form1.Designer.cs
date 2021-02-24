f69a3f17e77f9c1a32512043280d3275e127237c
Wafi Azmi Hartono <wafiazmi.ahartono@gmail.com>
2021-02-24 08:19:23 +0700

    Menambahkan program cipher transposition

---
+++ TranspositionCipher/TranspositionCipher/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TranspositionCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.radioButtonEncrypt = new System.Windows.Forms.RadioButton();
+            this.radioButtonDecrypt = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(73, 23);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(146, 20);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(73, 59);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(146, 20);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(144, 131);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 2;
+            this.buttonGo.Text = "GO";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // radioButtonEncrypt
+            // 
+            this.radioButtonEncrypt.AutoSize = true;
+            this.radioButtonEncrypt.Location = new System.Drawing.Point(242, 62);
+            this.radioButtonEncrypt.Name = "radioButtonEncrypt";
+            this.radioButtonEncrypt.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonEncrypt.TabIndex = 3;
+            this.radioButtonEncrypt.TabStop = true;
+            this.radioButtonEncrypt.Text = "Encrypt";
+            this.radioButtonEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDecrypt
+            // 
+            this.radioButtonDecrypt.AutoSize = true;
+            this.radioButtonDecrypt.Location = new System.Drawing.Point(327, 62);
+            this.radioButtonDecrypt.Name = "radioButtonDecrypt";
+            this.radioButtonDecrypt.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonDecrypt.TabIndex = 4;
+            this.radioButtonDecrypt.TabStop = true;
+            this.radioButtonDecrypt.Text = "Decrypt";
+            this.radioButtonDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Key";
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(289, 23);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(100, 20);
+            this.textBoxKey.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 166);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonDecrypt);
+            this.Controls.Add(this.radioButtonEncrypt);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Transpose Cipher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.RadioButton radioButtonEncrypt;
+        private System.Windows.Forms.RadioButton radioButtonDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxKey;
+    }
+}
+
---
+++ TranspositionCipher/TranspositionCipher/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TranspositionCipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButtonEncrypt.Checked = true;
+            textBoxInput.Focus();
+            textBoxKey.Text = "pangram";
+
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            string key = textBoxKey.Text;
+            string input = textBoxInput.Text;
+            string output = "";
+
+            if (radioButtonEncrypt.Checked == true)
+            {
+                output = Encipher(input, key, '-');
+            }
+            else
+            {
+                output = Decipher(input, key);
+            }
+
+            textBoxOutput.Text = output;
+            
+        }
+        private static int[] GetShiftIndexes(string key)
+        {
+            int keyLength = key.Length;
+            int[] indexes = new int[keyLength];
+            List<KeyValuePair<int, char>> sortedKey = new List<KeyValuePair<int, char>>();
+            int i;
+
+            for (i = 0; i < keyLength; ++i)
+                sortedKey.Add(new KeyValuePair<int, char>(i, key[i]));
+
+            sortedKey.Sort(
+                delegate (KeyValuePair<int, char> pair1, KeyValuePair<int, char> pair2) {
+                    return pair1.Value.CompareTo(pair2.Value);
+                }
+            );
+
+            for (i = 0; i < keyLength; ++i)
+                indexes[sortedKey[i].Key] = i;
+
+            return indexes;
+        }
+
+        public static string Encipher(string input, string key, char padChar)
+        {
+            input = (input.Length % key.Length == 0) ? input : input.PadRight(input.Length - (input.Length % key.Length) + key.Length, padChar);
+            StringBuilder output = new StringBuilder();
+            int totalChars = input.Length;
+            int totalColumns = key.Length;
+            int totalRows = (int)Math.Ceiling((double)totalChars / totalColumns);
+            char[,] rowChars = new char[totalRows, totalColumns];
+            char[,] colChars = new char[totalColumns, totalRows];
+            char[,] sortedColChars = new char[totalColumns, totalRows];
+            int currentRow, currentColumn, i, j;
+            int[] shiftIndexes = GetShiftIndexes(key);
+
+            for (i = 0; i < totalChars; ++i)
+            {
+                currentRow = i / totalColumns;
+                currentColumn = i % totalColumns;
+                rowChars[currentRow, currentColumn] = input[i];
+            }
+
+            for (i = 0; i < totalRows; ++i)
+                for (j = 0; j < totalColumns; ++j)
+                    colChars[j, i] = rowChars[i, j];
+
+            for (i = 0; i < totalColumns; ++i)
+                for (j = 0; j < totalRows; ++j)
+                    sortedColChars[shiftIndexes[i], j] = colChars[i, j];
+
+            for (i = 0; i < totalChars; ++i)
+            {
+                currentRow = i / totalRows;
+                currentColumn = i % totalRows;
+                output.Append(sortedColChars[currentRow, currentColumn]);
+            }
+
+            return output.ToString();
+        }
+
+        public static string Decipher(string input, string key)
+        {
+            StringBuilder output = new StringBuilder();
+            int totalChars = input.Length;
+            int totalColumns = (int)Math.Ceiling((double)totalChars / key.Length);
+            int totalRows = key.Length;
+            char[,] rowChars = new char[totalRows, totalColumns];
+            char[,] colChars = new char[totalColumns, totalRows];
+            char[,] unsortedColChars = new char[totalColumns, totalRows];
+            int currentRow, currentColumn, i, j;
+            int[] shiftIndexes = GetShiftIndexes(key);
+
+            for (i = 0; i < totalChars; ++i)
+            {
+                currentRow = i / totalColumns;
+                currentColumn = i % totalColumns;
+                rowChars[currentRow, currentColumn] = input[i];
+            }
+
+            for (i = 0; i < totalRows; ++i)
+                for (j = 0; j < totalColumns; ++j)
+                    colChars[j, i] = rowChars[i, j];
+
+            for (i = 0; i < totalColumns; ++i)
+                for (j = 0; j < totalRows; ++j)
+                    unsortedColChars[i, j] = colChars[i, shiftIndexes[j]];
+
+            for (i = 0; i < totalChars; ++i)
+            {
+                currentRow = i / totalRows;
+                currentColumn = i % totalRows;
+                output.Append(unsortedColChars[currentRow, currentColumn]);
+            }
+
+            return output.ToString();
+        }
+    }
+}
