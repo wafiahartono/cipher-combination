9381379a486f958edb47cb2207d5e15da213d2e5
Wafi Azmi Hartono <wafiazmi.ahartono@gmail.com>
2021-02-27 00:05:32 +0700

    Memperbarui cipher additive untuk menggunakan LINQ

--- AdditiveCipher/AdditiveCipher.cs
+++ AdditiveCipher/AdditiveCipher.cs
@@ -1,4 +1,5 @@
-﻿using Common;
+﻿using System.Linq;
+using Common;
 
 namespace AdditiveCipher
 {
@@ -11,32 +12,10 @@ namespace AdditiveCipher
             _key = key;
         }
 
-        public override string Encrypt(string plaintext)
-        {
-            char[] ciphertext = new char[plaintext.Length];
-            for (int i = 0; i < plaintext.Length; i++)
-            {
-                char pc = plaintext[i];
-                char cc = (char) (Utils.Modulo(pc - 32 + _key, 95) + 32);
-                ciphertext[i] = cc;
-                // Console.WriteLine($"{(int) pc:D3} {(int) pc:X3} {pc} > {(int) cc:D3} {(int) cc:X3} {cc}");
-            }
+        public override string Encrypt(string plaintext) => string.Join(null,
+            from c in plaintext select (char) (Utils.Modulo(c - 32 + _key, 95) + 32));
 
-            return new string(ciphertext);
-        }
-
-        public override string Decrypt(string ciphertext)
-        {
-            char[] plaintext = new char[ciphertext.Length];
-            for (int i = 0; i < ciphertext.Length; i++)
-            {
-                char cc = ciphertext[i];
-                char pc = (char) (Utils.Modulo(cc - 32 - _key, 95) + 32);
-                plaintext[i] = pc;
-                // Console.WriteLine($"{(int) cc:D3} {(int) cc:X3} {cc} > {(int) pc:D3} {(int) pc:X3} {pc}");
-            }
-
-            return new string(plaintext);
-        }
+        public override string Decrypt(string ciphertext) => string.Join(null,
+            from c in ciphertext select (char) (Utils.Modulo(c - 32 - _key, 95) + 32));
     }
 }
